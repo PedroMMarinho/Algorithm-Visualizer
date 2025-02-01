0fd92041203a58f32391ff5260c6acf134626296
Pedro Marinho <pmiguel.marinho@gmail.com>
2025-02-01 12:15:37 +0000

    Slider, run and compile elements complete;Moving to file addition and CodeEditor

--- frontend/src/CodeSettings/CodeSettings.jsx
+++ frontend/src/CodeSettings/CodeSettings.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import style from "./CodeSettings.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,6 +8,9 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function CodeSettings() {
+
+  const [speed, setSpeed] = useState(1);
+
   return (
     <div className={style.container}>
       <div className={style.element}>
@@ -22,8 +26,23 @@ function CodeSettings() {
         </button>
       </div>
       <div className={style.element}>
+        
+        <div className={style.sliderContainer}>
         <FontAwesomeIcon icon={faGaugeHigh} className={style.icon} />
-        Speed (Slider)
+          <label htmlFor="speedSlider" className={style.sliderLabel}>
+          Speed ({`${speed.toFixed(1)}x`})
+          </label>
+          <input
+            type="range"
+            id="speedSlider"
+            min="0.1"
+            max="2.0"
+            step="0.1"
+            value={speed}
+            onChange={(e) => setSpeed(parseFloat(e.target.value))}
+            className={style.slider}
+          />
+        </div>
       </div>
     </div>
   );
